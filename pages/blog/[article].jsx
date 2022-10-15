430b57d601ce72aa68d1ab5ebc75fa9e6887dabc
AJ <ancheetah@users.noreply.github.com>
2022-10-15 14:23:00 -0400

    article styles; render image and author

--- pages/blog/[article].jsx
+++ pages/blog/[article].jsx
@@ -1,16 +1,17 @@
-
 import {
     builder,
     BuilderComponent,
     BuilderContent,
     useIsPreviewing,
-  } from "@builder.io/react";
-  import Head from "next/head";
-  import DefaultErrorPage from "next/error";
-  import React from "react";
+    Image,
+  } from '@builder.io/react'
+  import Head from 'next/head'
+  import DefaultErrorPage from 'next/error'
+  import React from 'react'
+  import { Themed } from 'theme-ui'
   
   function BlogArticle({ article }) {
-    const isPreviewing = useIsPreviewing();
+    const isPreviewing = useIsPreviewing()
     if (!article && !isPreviewing) {
       return (
         <>
@@ -19,7 +20,7 @@ import {
           </Head>
           <DefaultErrorPage statusCode={404} />
         </>
-      );
+      )
     }
   
     return (
@@ -29,54 +30,70 @@ import {
         model="blog-article"
       >
         {(data, loading, fullContent) => (
-          <React.Fragment>
-            <Head>
-              {/* Render meta tags from custom field */}
-              <title>{data?.title}</title>
-              <meta name="description" content={data?.blurb} />
-              <meta name="og:image" content={data?.image} />
-            </Head>
+            <React.Fragment>
+              <Head>
+                {/* Render meta tags from custom field */}
+                <title>{data?.title}</title>
+                <meta name="description" content={data?.blurb} />
+                <meta name="og:image" content={data?.image} />
+              </Head>
   
-            <div>
-              <div>{data?.title}</div>
+              <Themed.div
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                }}
+              >
+                <Themed.h2 sx={{ color: 'primary', textAlign: 'center' }}>
+                  {data?.title}
+                </Themed.h2>
+                <Themed.div sx={{ position: 'relative', maxHeight: 400 }}>
+                  <Image aspectRatio={0.4} image={data?.image} />
+                </Themed.div>
+                <Themed.h6 sx={{ color: 'secondary', textAlign: 'center' }}>
+                  By {data?.author.value.name}
+                </Themed.h6>
+              </Themed.div>
               {/* Render the Builder drag/drop'd content */}
               <BuilderComponent
                 name="blog-article"
                 content={fullContent}
                 options={{ includeRefs: true }}
               />
-            </div>
-          </React.Fragment>
-        )}
+            </React.Fragment>
+          )
+        }
       </BuilderContent>
-    );
+    )
   }
   
   export async function getStaticProps({ params }) {
-    const article = await builder
-      .get("blog-article", {
-        // Include references, like our `author` ref
-        options: { includeRefs: true },
-        query: {
-          // Get the specific article by handle
-          "data.handle": params.handle,
-        },
-      })
-      .promise() || null;
+    const article =
+      (await builder
+        .get('blog-article', {
+          // Include references, like our `author` ref
+          options: { includeRefs: true },
+          query: {
+            // Get the specific article by handle
+            'data.handle': params.handle,
+          },
+        })
+        .promise()) || null
   
     return {
       props: {
         article,
       },
-    };
+    }
   }
   
   export async function getStaticPaths() {
     return {
       paths: [],
       fallback: true,
-    };
+    }
   }
   
-  export default BlogArticle;
+  export default BlogArticle
   
