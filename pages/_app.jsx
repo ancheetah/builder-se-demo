2be00fe33ddf0f3db65997e813db672d237dcd57
AJ <ancheetah@users.noreply.github.com>
2022-10-17 12:22:34 -0400

    app jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,12 @@
 import '@assets/main.css'
-import type { AppProps } from 'next/app'
+// import type { AppProps } from 'next/app'
 import { builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 import Layout from '@components/common/Layout'
 
 builder.init(builderConfig.apiKey)
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Layout pageProps={pageProps}>
